9a2af9e3574d05e599504d32053942359e027efc
sobird <i@sobird.me>
2023-11-18 22:14:16 +0800

    typescript-react

--- src/standard.ts
+++ src/standard.ts
@@ -3,8 +3,13 @@ import { type Linter } from 'eslint';
 const config: Linter.Config = {
   extends: ['eslint:recommended'],
   rules: {
-    // todo
+    // 使用单引号，字符串中包含了一个其它引号 允许 "a string containing 'single' quotes"
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    // console.log 警告提示
     'no-console': 'warn',
+    // 不允许连续空格
+    'no-multi-spaces': 'error',
+    'linebreak-style': ['error', 'unix'],
   }
 };
 
