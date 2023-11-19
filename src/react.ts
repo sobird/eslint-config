109622f8359a89e98b025a08069c07da31c7fff0
sobird <i@sobird.me>
2023-11-19 22:26:24 +0800

    eslint-config

---
+++ src/react.ts
@@ -0,0 +1,14 @@
+export default {
+  extends: ['plugin:react/recommended'],
+  plugins: [
+    'react',
+  ],
+  parserOptions: {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'useJSXTextNode': true,
+  },
+  settings: {},
+  rules: {}
+};
