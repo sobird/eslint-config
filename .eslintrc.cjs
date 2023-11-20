8309d2698a6e2ea182778d06788d6871e9c570df
sobird <i@sobird.me>
2023-11-20 22:13:39 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,17 @@
  * "off" or 0 - turn the rule off
  * "warn" or 1 - turn the rule on as a warning (doesn't affect exit code)
  * "error" or 2 - turn the rule on as an error (exit code will be 1)
- * 
+ *
  * @see https://typescript-eslint.io/play
  */
 
 module.exports = {
-  'overrides': [
+  extends: './dist/base.cjs',
+  overrides: [
     {
-      'files': ['*.ts'],
-      'extends': './dist/typescript.cjs',
+      files: ['*.ts'],
+      extends: './dist/typescript.cjs',
     },
-  ]
+  ],
+  rules: {},
 };
---
+++ src/base.ts
@@ -0,0 +1,10 @@
+// import { type Linter } from 'eslint';
+
+const config = {
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {},
+};
+
+export default config;
