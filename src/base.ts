23f436891b284cc23efc7aa9685938eda42abdc1
sobird <i@sobird.me>
2023-11-21 23:22:08 +0800

    reset base

--- src/base.ts
+++ src/base.ts
@@ -1,9 +1,6 @@
 // import { type Linter } from 'eslint';
 
 const config = {
-  extends: [
-    'airbnb-base',
-  ],
   rules: {
     // 关闭
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
