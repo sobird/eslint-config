7ae21ae19053f132bcd4202f481c0f6833d2e4ba
sobird <i@sobird.me>
2023-11-22 23:33:16 +0800

    max-len: 120

--- src/base.ts
+++ src/base.ts
@@ -2,9 +2,27 @@
 
 const config = {
   rules: {
-    // 关闭
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+    /** style: begin */
+    'arrow-body-style': ['error', 'always'],
+    // specify the maximum length of a line in your program
+    // https://eslint.org/docs/rules/max-len
+    'max-len': ['error', {
+      code: 120,
+      tabWidth: 2,
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+    /** style: end */
+
+    /** import: begin */
     'import/extensions': 'off',
+    'import/no-import-module-exports': ['error', {
+      exceptions: ['**/*'],
+    }],
+    /** import: end */
   },
 };
 
