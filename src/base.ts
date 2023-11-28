a74587e5280ab2176d04012c43f6fe957fc8d5be
sobird <i@sobird.me>
2023-11-28 10:25:54 +0800

    next.js

--- src/base.ts
+++ src/base.ts
@@ -46,20 +46,6 @@ const config = {
     }],
     'import/no-webpack-loader-syntax': 'warn',
     /** import: end */
-
-    // 非 required 的 prop 必须有 defaultProps
-    // @off 不强制要求写 defaultProps
-    'react/require-default-props': 'off',
-    // 防止React被错误地标记为未使用
-    'react/jsx-uses-react': 'error',
-    // https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports
-    'react/react-in-jsx-scope': 'off',
-    'react/no-unstable-nested-components': ['error', {
-      allowAsProps: true,
-    }],
-    'react-hooks/exhaustive-deps': 'off',
-    // 禁用
-    'react/jsx-props-no-spreading': 'off',
   },
 };
 
