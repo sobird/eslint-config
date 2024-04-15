0e8a70d22797be8c9d9cb6da1fd7bd27b4e150a6
sobird <i@sobird.me>
2024-04-15 11:45:14 +0800

    update

--- src/base.ts
+++ src/base.ts
@@ -4,13 +4,24 @@ const config = {
   extends: [
     'airbnb-base',
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   rules: {
-    /** style: begin */
+    // style
+    // 要求箭头函数体使用大括号(Require braces around arrow function bodies)
+    // https://eslint.org/docs/latest/rules/arrow-body-style
     'arrow-body-style': ['error', 'always'],
-    // specify the maximum length of a line in your program
+
+    // 禁止使用一元操作符 ++ 和 --(disallow use of unary operators, ++ and --)
+    // https://eslint.org/docs/rules/no-plusplus
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+
+    // 代码行最大长度(specify the maximum length of a line in your program)
     // https://eslint.org/docs/rules/max-len
     'max-len': ['error', {
-      code: 120,
+      code: 150,
       tabWidth: 2,
       ignoreUrls: true,
       ignoreComments: true,
@@ -18,39 +29,61 @@ const config = {
       ignoreStrings: true,
       ignoreTemplateLiterals: true,
     }],
+
+    // 要求使用一致的 return 语句(require return statements to either always or never specify values)
+    // https://eslint.org/docs/rules/consistent-return
     'consistent-return': 'off',
-    /** style: end */
 
-    /** errors: begin */
-    /** 不允许对函数参数赋值 */
-    'no-param-reassign': [
-      'error',
-      {
-        props: true,
-        ignorePropertyModificationsFor: [
-          'e', // for e.returnvalue
-          'ctx', // for Koa routing
-          'req', // for Express requests
-          'request', // for Express requests
-          'res', // for Express responses
-          'response', // for Express responses
-          'state', // for store state
-        ],
-      },
-    ],
-    // 对 for 循环禁用
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    /** errors: end */
+    // 禁止标识符中有悬空下划线(disallow dangling underscores in identifiers)
+    // https://eslint.org/docs/rules/no-underscore-dangle
+    'no-underscore-dangle': ['error', {
+      allow: ['__filename', '__dirname'],
+      allowAfterThis: false,
+      allowAfterSuper: false,
+      enforceInMethodNames: true,
+    }],
 
-    /** import: begin */
+    // best-practices
+    // 禁止对函数参数再赋值(Disallow reassigning function parameters)
+    // disallow parameter object manipulation except for specific exclusions
+    // rule: https://eslint.org/docs/rules/no-param-reassign.html
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'e', // for e.returnvalue
+        'ctx', // for Koa routing
+        'req', // for Express requests
+        'request', // for Express requests
+        'res', // for Express responses
+        'response', // for Express responses
+        'state', // for store state
+        'accu', // for reduce
+      ],
+    }],
+
+    // import
+    // Ensure consistent use of file extension within the import path
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
     'import/extensions': 'off',
+
+    // Reports the use of import declarations with CommonJS exports in any module except for the main module.
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-import-module-exports.md
     'import/no-import-module-exports': ['error', {
       exceptions: ['**/*'],
     }],
+
+    // Forbid Webpack loader syntax in imports
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-webpack-loader-syntax.md
     'import/no-webpack-loader-syntax': 'warn',
-    // 默认导出 不是必须的
+
+    // Require modules with a single export to use a default export
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
     'import/prefer-default-export': 'off',
-    /** import: end */
+
+    // 禁止无关的依赖(Forbid the use of extraneous packages)
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+    // paths are treated both as absolute paths, and relative to process.cwd()
+    'import/no-extraneous-dependencies': 'off',
   },
 };
 
