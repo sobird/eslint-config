acfe5dc2ef853f6e92d19491a15656aacd0e4798
sobird <i@sobird.me>
2026-02-03 18:04:05 +0800

    chore: add eslint stylistic

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { type Linter } from "eslint";
-import {type RulesConfig} from '@eslint/core'
+import { type RuleConfig} from '@eslint/core'
 
 import { javascript } from "./configs/javascript";
 
-export interface ConfigObject<Rules extends RulesConfig> extends Linter.Config<Rules> {
+export type WrapRuleConfig<T extends { [key: string]: any }> = {
+  [K in keyof T]: T[K] extends RuleConfig ? T[K] : RuleConfig<T[K]>
+}
+
+export type ExactProps<T> = {
+  [K in keyof T as 
+    string extends K ? never :
+    number extends K ? never :
+    symbol extends K ? never :
+    K
+  ]: T[K]
+};
+
+export interface ConfigObject<Rules extends Record<string, any>> extends Linter.Config<Rules> {
   languageOptions?: Linter.LanguageOptions
 }
 
