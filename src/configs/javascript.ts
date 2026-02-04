ff2ecaa215a76f9d06f2a65b5826b7688da4e34b
sobird <i@sobird.me>
2026-02-04 20:33:51 +0800

    chore: set off no-inline-comments,@stylistic/line-comment-position

--- src/configs/javascript.ts
+++ src/configs/javascript.ts
@@ -131,7 +131,7 @@ export const rules: Partial<ESLintRules> = {
   'no-implicit-globals': 'error',
   'no-implied-eval': 'error',
   'no-import-assign': 'error',
-  'no-inline-comments': 'error',
+  'no-inline-comments': 'off',
   'no-inner-declarations': 'error',
   'no-invalid-regexp': 'error',
   'no-invalid-this': 'error',
