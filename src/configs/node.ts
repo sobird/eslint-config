9d6115f341c868934a704699a22e51e6866fcbf0
sobird <i@sobird.me>
2026-02-05 12:48:55 +0800

    fix: set off no-return-await (deprecated)

--- src/configs/node.ts
+++ src/configs/node.ts
@@ -10,7 +10,7 @@ export function node(): Configs['flat/all'][] {
       rules: {
         'node/callback-return': 'error',
         'node/exports-style': 'error',
-        'node/file-extension-in-import': 'error',
+        'node/file-extension-in-import': ['error', 'never'],
         'node/global-require': 'error',
         'node/handle-callback-err': ['error', '^(err|error)$'],
         'node/no-callback-literal': 'error',
@@ -18,8 +18,9 @@ export function node(): Configs['flat/all'][] {
         'node/no-exports-assign': 'error',
         'node/no-extraneous-import': 'error',
         'node/no-extraneous-require': 'error',
-        'node/no-missing-import': 'error',
-        'node/no-missing-require': 'error',
+        // see import/no-unresolved
+        'node/no-missing-import': 'off',
+        'node/no-missing-require': 'off',
         'node/no-mixed-requires': 'error',
         'node/no-new-require': 'error',
         'node/no-path-concat': 'error',
@@ -28,9 +29,17 @@ export function node(): Configs['flat/all'][] {
         'node/no-restricted-import': 'error',
         'node/no-restricted-require': 'error',
         'node/no-sync': 'error',
-        'node/no-top-level-await': 'error',
+        // 'node/no-top-level-await': 'error',
         'node/no-unpublished-bin': 'error',
-        'node/no-unpublished-import': 'error',
+        'node/no-unpublished-import': [
+          'error',
+          {
+            ignorePrivate: true,
+            ignoreTypeImport: true,
+            tryExtensions: ['.js', '.ts', '.tsx', '.json'],
+            allowModules: [],
+          },
+        ],
         'node/no-unpublished-require': 'error',
         'node/no-unsupported-features/es-builtins': 'error',
         'node/no-unsupported-features/es-syntax': 'error',
@@ -50,6 +59,14 @@ export function node(): Configs['flat/all'][] {
         // 废弃规则也添加前缀
         'node/no-hide-core-modules': 'error',
         'node/shebang': 'error',
+      },
+    },
+
+    {
+      files: ['**/*.test.ts', '**/*.test.js', 'scripts/**/*'],
+      rules: {
+        // 测试/脚本文件不检查「未发布导入」（因为这些文件本身不会被发布）
+        'node/no-unpublished-import': 'off',
       },
     },
   ];
