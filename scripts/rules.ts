447438f90cf08f6c222026e39b127d12766ab5a0
sobird <i@sobird.me>
2026-02-07 00:21:58 +0800

    chore: add node rule options ts tool

--- scripts/rules.ts
+++ scripts/rules.ts
@@ -5,16 +5,18 @@ import path from 'node:path';
 import { compile } from 'json-schema-to-typescript';
 
 import eslint from './rules/eslint';
+import node from './rules/node';
 
 const dir = path.join('types', 'rules');
 
 await fs.mkdir(dir, { recursive: true });
 
 
-for (const item of [eslint]) {
+for (const item of [eslint, node]) {
   const { schema, name } = item;
 
-  const result = await compile(schema, '', { bannerComment: '/* eslint-disable */' });
-
-  await fs.writeFile(path.join(dir, `${name}.d.ts`), result);
+  if (schema !== undefined) {
+    const result = await compile(schema, '', { bannerComment: '/* eslint-disable */' });
+    await fs.writeFile(path.join(dir, `${name}.d.ts`), result);
+  }
 }
