1262156e99e61173a277f4e83dde3ec1b14ef3b0
sobird <i@sobird.me>
2026-02-07 20:30:29 +0800

    chore: is it necessary to keep the rule prefix consistent with the community?

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,10 @@
 // https://eslint.org/docs/latest/use/configure/configuration-files
 
-import { defineConfig } from 'eslint/config';
-import importPlugin from 'eslint-plugin-import';
-
 import { sobird } from './src';
 
-export default defineConfig(
-  // {
-  //   files: ['**/*.{ts,tsx}'],
-  //   extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
-  //   // other configs...
-  // },
-  sobird({
-    typescript: true,
-    rules: {
-      '@typescript/no-explicit-any': [{ fixToUnknown: true }],
-    },
-  }),
-);
+export default sobird({
+  typescript: true,
+  rules: {
+    '@typescript/no-explicit-any': [{ fixToUnknown: true }],
+  },
+});
--- scripts/rules/eslint/index.ts
+++ scripts/rules/eslint/index.ts
@@ -4,7 +4,6 @@ import versions from './versions.json';
 import type { JSONSchema4 } from 'json-schema';
 import { rulesToJSONSchema } from 'scripts/utils';
 
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   name: 'eslint',
@@ -15,7 +14,6 @@ export default {
 
     const entries = Array.from(rules, ([ruleName, ruleModel]) => {
       if (ruleName in added) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
         const version = added[ruleName as keyof typeof added];
         Object.assign(ruleModel.meta ?? {}, { version });
       }
